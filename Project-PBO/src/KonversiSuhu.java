d698fdd9e4a23245e6d05bf270e6206a47895177
Alieff215 <155336577+alieff215@users.noreply.github.com>
2024-01-08 09:07:12 +0700

    Delete Project-PBO directory

--- Project-PBO/src/KonversiSuhu.java
+++
@@ -1,5 +0,0 @@
-abstract class KonversiSuhu {
-    abstract double konversiKeCelsius(double suhu);
-    abstract double konversiKeFahrenheit(double suhu);
-    abstract double konversiKeKelvin(double suhu);
-}
